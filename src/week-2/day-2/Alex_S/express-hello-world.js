e88d59f2f0f64e8192b99cd3ac2b1bfda8305e49
Alex Sakata <solerjapan@gmail.com>
2021-08-24 16:45:10 -0400

    completed version

--- src/week-2/day-2/Alex_S/express-hello-world.js
+++ src/week-2/day-2/Alex_S/express-hello-world.js
@@ -1,7 +1,5 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var app = express_1["default"]();
+var express = require('express');
+var app = express();
 var port = 3000;
 app.get('/', function (req, res) {
     res.send('Hello World!');
--- src/week-2/day-2/Alex_S/express-hello-world.ts
+++ src/week-2/day-2/Alex_S/express-hello-world.ts
@@ -1,7 +1,5 @@
-import express from 'express'
-
+const express = require('express')
 const app = express()
-
 const port = 3000
 
 app.get('/', (req, res) => {
