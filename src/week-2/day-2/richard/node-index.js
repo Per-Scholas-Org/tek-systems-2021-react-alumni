f22160d444177560c4a1786a70f24893ecf0b15c
Richard Grant <rgranyny@gmail.com>
2021-08-25 09:33:21 -0400

    I have added the node and express ts and js files

---
+++ src/week-2/day-2/richard/node-index.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var hostname = '127.0.0.1';
+var port = 3000;
+var server = http.createServer(function (request, response) {
+    request.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('Hello World!!');
+});
+server.listen(port, hostname, function () {
+    console.log('Server running at http://${hostname}:${port}/');
+});
