5c8f35bbc1cd2dd4c575cf7a97897dc02a9c5f04
Alex S <55304780+SolerJapan@users.noreply.github.com>
2021-08-27 13:40:19 -0400

    Revert "Express rest crud alex s"

--- src/week1/Alex_S/day2/test.js
+++
@@ -1,3 +0,0 @@
-
-var SUNNY_DAY_MESSAGE = "it is beutiful and sunny today!";
-console.log(typeof SUNNY_DAY_MESSAGE);
--- src/week1/Alex_S/day2/test.ts
+++
@@ -1,9 +0,0 @@
-function (numMinutes: number, numHours) 
-{ 
-    return numHours * 60 + numMinutes; 
-}
-
-const SUNNY_DAY_MESSAGE = "it is beutiful and sunny today!";
-
-console.log(typeof SUNNY_DAY_MESSAGE)
-
