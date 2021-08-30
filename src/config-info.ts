2b9cc874a79b1b12af2f6d3b7daeefdcc694dc69
Alex Sakata <solerjapan@gmail.com>
2021-08-30 16:46:36 -0400

    update

---
+++ src/config-info.ts
@@ -0,0 +1,15 @@
+const PORT = 9999;
+
+// same as getPort - but this is a function expression
+const getPortAlternate = function () {
+    return PORT;
+}
+
+// arrow function
+const getPort = () => {
+    return PORT;
+}
+
+export {
+    getPort,
+}
